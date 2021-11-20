3c2c86fdc2d95bb36577cddf6a227b976564153e
baron1994 <1942992735@qq.com>
2021-11-21 00:51:07 +0800

    chapter1 初始化项目 inconfont

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { GlobalStyle } from "./style";
+import { IconStyle } from "./assets/iconfont/iconfont";
 
 function App() {
   return (
     <div>
-     app
+      <GlobalStyle></GlobalStyle>
+      <IconStyle></IconStyle>
+      <i className="iconfont">&#xe62b;</i>
     </div>
   );
 }
